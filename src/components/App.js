b32e7352f7ca9f2c33ac215b70db4c176af8acf3
CKURZ <ckurz098@gmail.com>
2022-04-07 11:41:59 -0700

    init

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,35 @@ import Player from './Player';
 function App() {
 
   // const [keysPressed, setKeyPressed] = useState([]);
-  let keysPressed = [];
+  const [keysPressed, setKeysPressed] = useState([]);
 
-  function logKeyDown(e) {
 
-    if (!keysPressed.includes(e.key)) {
-      keysPressed = [...keysPressed, e.key];
+    function logKeyDown(e) {
+
+      if (!keysPressed.includes(e.key)) {
+        setKeysPressed([...keysPressed, e.key]);
+        console.log('Pressed: ', keysPressed);
+      }
+  
+
+    }
+
+
+    function logKeyUp(e) {
+      const newKeys = keysPressed.filter(key => key !== e.key);
+      if (newKeys !== keysPressed) setKeysPressed(newKeys);
       console.log('Pressed: ', keysPressed);
     }
-  
-  }
 
 
-  function logKeyUp(e) {
-    const newKeys = keysPressed.filter(key => key !== e.key);
-    if (newKeys !== keysPressed) keysPressed = newKeys;
-    console.log('Pressed: ', keysPressed);
-  }
 
   console.log('Pressed: ', keysPressed);
 
   return (
     <div id='main-window'
       tabIndex={-1}
-      // onKeyUp={logKeyUp}
-      // onKeyDown={logKeyDown}
+      onKeyUp={logKeyUp}
+      onKeyDown={logKeyDown}
 
       keclassName="App">
       <h1>Press W: Up A:Left D:Right</h1>
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -6,18 +6,15 @@ import React, { useState, useEffect } from 'react';
 let  playerPosition = { x: 0, y: 0, dir: 90 };
 let  playerMovement = { speed: 3, currentSpeed: 0, topSpeed: 10, slowDown: 2 };
 
-let  currentKeys = [];
-
-const Player = ({  }) => {
-
-
+const Player = ({  keysPressed }) => {
   
 function testLoop (){
 
     let { x, y, dir } = playerPosition;
     let { speed, currentSpeed, topSpeed, slowDown } = playerMovement;
 
-    if (currentKeys.includes('w')) {
+
+    if (keysPressed.includes('w')) {
         (currentSpeed < topSpeed) ? currentSpeed += speed : currentSpeed = topSpeed;
         x -= currentSpeed * Math.cos(dir * Math.PI / 180);
         y -= currentSpeed * Math.sin(dir * Math.PI / 180);
@@ -26,49 +23,26 @@ function testLoop (){
     }
 
 
-    if (currentKeys.includes('d')) {
+    if (keysPressed.includes('d')) {
         (dir < 360) ? dir += 2 : dir = 0;
         playerPosition = { ...playerPosition, dir: dir };
     }
-    if (currentKeys.includes('a')) {
+    if (keysPressed.includes('a')) {
         (dir <= 0) ? dir = 360 : dir -= 2;
         playerPosition = { ...playerPosition, dir: dir };
     }
 
 
   console.log('POSITION: ',playerPosition);
-  console.log('CURR: ',currentKeys);
-    setTimeout(testLoop, 16.67)
+  console.log('CURR: ',keysPressed);
+    // setTimeout(testLoop, 16.67)
 }
 
-   testLoop();
+ 
 
-// useEffect(() => {
-// currentKeys = keysPressed;
-// testLoop()
-// }, [keysPressed])
-
-
-
-
-function logKeyDown(e) {
-
-  if (!currentKeys.includes(e.key)) {
-    currentKeys = [...currentKeys, e.key];
-    console.log('Pressed: ', currentKeys);
-  }
-
-}
-
-
-function logKeyUp(e) {
-  const newKeys = currentKeys.filter(key => key !== e.key);
-  if (newKeys !== currentKeys) currentKeys = newKeys;
-  console.log('Pressed: ', currentKeys);
-}
-
-console.log('CurrentKeys: ',currentKeys);
-
+useEffect(() => {
+testLoop()
+}, [keysPressed])
 
 
 return (
@@ -76,9 +50,7 @@ return (
         id='player-object'
         alt='player-sprite'
         tabIndex={-1}
-        onKeyUp={logKeyUp}
-        onKeyDown={logKeyDown}
-  
+
         src={require('../../assets/player_sprt.png')}
         style={{ "top": `${playerPosition.y}px`, "left": `${playerPosition.x}px`, "transform": `rotate(${playerPosition.dir - 90}deg)` }}
     />
