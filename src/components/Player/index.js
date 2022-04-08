e02a050be3018ea72855bf5014fec416f9729ecb
CKURZ <ckurz098@gmail.com>
2022-04-07 18:15:45 -0700

    Created correct force and velocity

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 
-const Player = ({gameSpeed}) => {
+const Player = ({ gameSpeed }) => {
 
   /* 
   This is the players state. In the actual build we need to make these states global so that every element can have access to them for collision detection
@@ -9,56 +9,44 @@ const Player = ({gameSpeed}) => {
   Description of all states:
   - x & y: These are the x and y cordinates. (0,0) is the TOP LEFT corner of the screen. I set the to 500x500 so they dont start in the corner
   - dir: this is the direction in degrees of the players ship
-  - speed: This dictates acceleration speed
-  - currentSpeed: This is exactley what it says it is.
-  - topSpeed: This is also just what it says. Its the top speed the player can move
-  - slowDown: This dictates the speed at which the player deaccelerates, could also be called friction.
+  - force: This dictates acceleration in a direction
+  - tx and ty: thrust magintueds for x and y
   - turnSpeed: the rate in pixles the player can turn
 
   NOTE: x and y account for the TOP LEFT corner of the player image. We can compensate for this by adding half the width and half the height of the image to these values
+
   */
-  const [playerPosition, setPlayerPosition] = useState({ x: 500, y: 500, dir: 90, speed: .1, currentSpeed: 0, topSpeed: 10, slowDown: .05, turnSpeed: 3 });
+  const [playerPosition, setPlayerPosition] = useState({ x: 500, y: 500, dir: 90, thrust: .05, tx: 0, ty: 0, turnSpeed: 2 });
 
+  //pull out those states
+  let { x, y, dir, thrust, tx, ty, turnSpeed } = playerPosition;
 
-
-
-  let { x, y, dir, speed, currentSpeed, topSpeed, slowDown, turnSpeed } = playerPosition;
+  //this will hold the currently pressed keys
   let keysPressed = [];
 
   //main loop for updating player position
   function loop() {
     if (keysPressed.includes('w')) {
-      (currentSpeed < topSpeed) ? currentSpeed += speed : currentSpeed = topSpeed;
-      x -= currentSpeed * Math.cos(dir * Math.PI / 180);
-      y -= currentSpeed * Math.sin(dir * Math.PI / 180);
-
+      tx -= thrust * Math.cos((dir) * Math.PI / 180);
+      ty -= thrust * Math.sin((dir) * Math.PI / 180);
     }
 
-    if (!keysPressed.includes('w')) {
-      if (currentSpeed > 0) {
-        currentSpeed -= slowDown 
-      x -= currentSpeed * Math.cos(dir * Math.PI / 180);
-      y -= currentSpeed * Math.sin(dir * Math.PI / 180);
-    }else {
-       currentSpeed = 0;
-    }
+    //constatley update momentum
+    x += tx;
+    y += ty;
 
-    }
-
-
+    //Rotate ship whe A or D pressed
     if (keysPressed.includes('d')) {
       (dir < 360) ? dir += turnSpeed : dir = 0;
-     
-
     }
     if (keysPressed.includes('a')) {
       (dir <= 0) ? dir = 360 : dir -= turnSpeed;
-  
     }
 
+//update state
+    setPlayerPosition({ ...playerPosition, x: x, y: y, dir: dir });
 
-    setPlayerPosition({ ...playerPosition, x: x, y: y, dir: dir, currentSpeed: currentSpeed });
-
+    //loop the code every <gameSpeed>ms
     setTimeout(() => {
       loop()
     }, gameSpeed)
@@ -66,8 +54,7 @@ const Player = ({gameSpeed}) => {
 
 
 
-//keyboard key event handlers. Keeps an array of all currently pressed keys
-
+  //keyboard key event handlers. Keeps an array of all currently pressed keys
   function logKeyDown(e) {
     e.preventDefault()
     if (!keysPressed.includes(e.key)) {
@@ -81,23 +68,22 @@ const Player = ({gameSpeed}) => {
     const newKeys = keysPressed.filter(key => key !== e.key);
     if (newKeys !== keysPressed) keysPressed = newKeys;
     console.log('Released: ', keysPressed);
-
   }
 
 
-//when the component is mounted, we initiate event listeners for keyup and keydown and start the loop
+  //when the component is mounted, we initiate event listeners for keyup and keydown and start the loop
   useEffect(() => {
     document.addEventListener('keyup', logKeyUp);
     document.addEventListener('keydown', logKeyDown);
     loop();
   }, [])
 
-/*
-  NOTES ABOUT ROTATION:
-  math.cos and Math.sin consider a RIGHT to be 0 deg, where  "transform: rotate()" in CSS considers UP to be 0 degrees
-  We have to compensate for this by subtracting 90 from the players direction.
-  This works because "transform: rotate()" will acept negative numbers where sin and cos cannot take negative numbers
-*/
+  /*
+    NOTES ABOUT ROTATION:
+    math.cos and Math.sin consider a RIGHT to be 0 deg, where  "transform: rotate()" in CSS considers UP to be 0 degrees
+    We have to compensate for this by subtracting 90 from the players direction.
+    This works because "transform: rotate()" will acept negative numbers where sin and cos cannot take negative numbers
+  */
   return (
     <img
       id='player-object'
