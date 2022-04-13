cbcf532dffea3cc2aef15ec5e8498cd7d8eff7c4
Emily Dokken <dokkenemily@gmail.com>
2022-04-12 18:50:02 -0700

    add initial html

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>COPYRIGHT 2022 NASA</p>
+    </footer>
+  )
+}
+
+export default Footer;
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 const Start = () => {
   return (
-    <h1>ASTEROIDS</h1>
+    <main>
+      <div>
+        <h1>ASTEROIDS</h1>
+      </div>
+      <div>
+        <h3>leaderboard</h3>
+        <table>
+          <tr>
+            <td>L33TGAMR</td>
+            <td>PAPRSHRDR</td>
+            <td>EMDOK</td>
+            <td>KT</td>
+            <td>KURZ890</td>
+            <td>ANGALET</td>
+            <td>BEEKILLR</td>
+            <td>FFYOGURT</td>
+            <td>KILLRBRGR</td>
+          </tr>
+          <tr>
+            <td>500900</td>
+            <td>400890</td>
+            <td>400777</td>
+            <td>399999</td>
+            <td>383876</td>
+            <td>369870</td>
+            <td>200000</td>
+            <td>198708</td>
+            <td>187907</td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <a href='/'>Login</a>
+        <a href='/'>Signup</a>
+      </div>
+      <Footer />
+    </main>
   )
 };
 
