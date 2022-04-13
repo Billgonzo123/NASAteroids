090c9e28ff50535f17e803d5a3fab7b00df3f92c
Emily Dokken <dokkenemily@gmail.com>
2022-04-12 17:40:19 -0700

    move App.js to src directory and update imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css';
-import MainWindow from './MainWindow';
+import './App.css';
+import MainWindow from './components/MainWindow';
 
 
 
---
+++ client/src/pages/Start.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Start = () => {
+  return (
+    <h1>ASTEROIDS</h1>
+  )
+};
+
+export default Start;
