bd6793c58a284bd68a36984263b3fa8a67c994c2
Emily Dokken <dokkenemily@gmail.com>
2022-04-13 18:50:08 -0700

    add dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import MainWindow from './components/MainWindow';
-
 import Button from './components/Button';
 import Profile from './components/Profile';
 
