19a24b3710ac39546abfb8846a814f61ec4ece53
Chris <cangalet@gmail.com>
2022-04-14 16:25:29 -0700

    added Hud component to MainWindow, moved customTheme to App from StartPage

--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -1,50 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Leaderboard from "../components/Leaderboard";
-import Login from "../components/Login";
+import Hud from "../components/Hud";
 import LevelLoadIn from "../components/LevelLoadIn";
 import Signup from "../components/Signup";
 import { Button, Box, Grid } from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Press Start 2P"],
-    body1: {
-      textTransform: "uppercase",
-    },
-  },
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    text: {
-      primary: "#FFFFFF",
-    },
-  },
-  components: {
-    MuiInput: {
-      styleOverrides: {
-        underline: {
-          "&&::before": {
-            borderBottom: "1px solid rgba(255, 255, 255, 1)"
-          },
-          "&&::after": {
-            borderBottom: "1px solid rgba(255, 255, 255, 1)"
-          }
-        }
-      }
-    }
-  }
-});
 
 const Start = () => {
   return (
-    <ThemeProvider theme={theme}>
+    
       <Box>
       <Grid
         container
@@ -57,12 +22,11 @@ const Start = () => {
         <div className="logo">
           <h1>ASTEROIDS</h1>
         </div>
-        <Leaderboard />
+        <Hud />
         {/* <Signup /> */}
         <Footer />
       </Grid>
     </Box>  
-   </ThemeProvider>
   );
 };
 
