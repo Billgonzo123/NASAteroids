589aa6367a0079b9414364b9e36c04156be91cda
Emily Dokken <dokkenemily@gmail.com>
2022-04-13 19:49:38 -0700

    add margin-top to logo

--- client/src/components/Leaderboard/index.js
+++ client/src/components/Leaderboard/index.js
@@ -1,24 +1,16 @@
 import * as React from "react";
-import {  Card,  CardActions,  CardContent,  Typography,} from "@mui/material";
+import {  Card, CardContent, Typography,} from "@mui/material";
 import LeaderboardTable from "../../components/LeaderboardTable";
 
 export default function BasicCard() {
   return (
     <Card sx={{ minWidth: 275, backgroundColor: "transparent" }}>
       <CardContent>
-        <Typography align="center" gutterBottom>
+        <Typography variant="h3" align="center" gutterBottom>
           Leaderboard
         </Typography>
         <LeaderboardTable />
       </CardContent>
-      <CardActions sx={{ justifyContent: "space-between"}}>
-        <button type="button" className="nes-btn upperCase">
-          Login
-        </button>
-        <button type="button" className="nes-btn upperCase">
-          Signup
-        </button>
-      </CardActions>
     </Card>
   );
 }
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -1,46 +1,54 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Leaderboard from "../components/Leaderboard";
-import { Button, Box, Grid } from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Card, CardActions, Button, Box, Grid } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'Press Start 2P',
-    ],
+    fontFamily: ["Press Start 2P"],
     body1: {
-      textTransform: 'uppercase',
+      textTransform: "uppercase",
     },
   },
   palette: {
-    text:{
-      primary: "#FFFFFF"
-    }
-  }
+    text: {
+      primary: "#FFFFFF",
+    },
+  },
 });
 
 const Start = () => {
   return (
     <ThemeProvider theme={theme}>
       <Box>
-      <Grid
-        container
-        columnSpacing={{ md: 1 }}
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        <div className="logo">
-          <h1>ASTEROIDS</h1>
-        </div>
-        <Leaderboard />
-        <Footer />
-      </Grid>
-    </Box>
+        <Grid
+          container
+          columnSpacing={{ md: 1 }}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          <div className="logo">
+            <h1>ASTEROIDS</h1>
+          </div>
+          <Leaderboard />
+          <Card sx={{ justifyContent: "space-between", backgroundColor: "transparent" }}>
+            <CardActions sx={{ justifyContent: "space-between", backgroundColor: "transparent" }}>
+              <button type="button" className="nes-btn upperCase">
+                Login
+              </button>
+              <button type="button" className="nes-btn upperCase">
+                Signup
+              </button>
+            </CardActions>
+          </Card>
+
+          <Footer />
+        </Grid>
+      </Box>
     </ThemeProvider>
-    
   );
 };
 
