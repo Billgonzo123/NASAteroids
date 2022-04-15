9088c20bcebd21e4f25a711380fd41df3283ddc5
CKURZ <ckurz098@gmail.com>
2022-04-15 08:42:58 -0700

    Fixed bug with asteroids popping in

--- client/src/components/MainWindow/index.js
+++ client/src/components/MainWindow/index.js
@@ -158,32 +158,36 @@ const MainWindow = ({ menuSoundstate, setMenuSoundState }) => {
         return pos.alive ? (
           //our asteroid has the potential to reneder at 4 places at once, so we need 4 elements per asteroid!
           <>
-            <img
-              key={posId}
-              id='asteroid-object'
-              alt='asteroid-sprite'
-              src={require('../../assets/img/asteroid_large_sprt.png')}
-              style={motion(pos.x, pos.y, pos.dir)} />
-            <img
-              key={posId + 100}
-              id='asteroid-shadow'
-              alt='asteroid-sprite'
-              src={require('../../assets/img/asteroid_large_sprt.png')}
-              style={motion(pos.xB, pos.y, pos.dir)} />
-            <img
-              key={posId + 200}
-              id='asteroid-shadow'
-              alt='asteroid-sprite'
-              src={require('../../assets/img/asteroid_large_sprt.png')}
-              style={motion(pos.x, pos.yB, pos.dir)} />
-            <img
-              key={posId + 300}
-              id='asteroid-shadow'
-              alt='asteroid-sprite'
-              src={require('../../assets/img/asteroid_large_sprt.png')}
-              style={motion(pos.xB, pos.yB, pos.dir)} />
+          <img
+            key={posId}
+            id='asteroid-object'
+            alt='asteroid-sprite'
+            src={require('../../assets/img/asteroid_large_sprt.png')}
+            style={motion(pos.x, pos.y, pos.dir)} />
 
-          </>
+          {pos.xB !== pos.x && <img
+            key={posId + 'a'}
+            id='asteroid-shadow'
+            alt='asteroid-sprite'
+            src={require('../../assets/img/asteroid_large_sprt.png')}
+            style={motion(pos.xB, pos.y, pos.dir)} />}
+
+          {pos.yB !== pos.y && <img
+            key={posId + 'b'}
+            id='asteroid-shadow'
+            alt='asteroid-sprite'
+            src={require('../../assets/img/asteroid_large_sprt.png')}
+            style={motion(pos.x, pos.yB, pos.dir)} />}
+
+
+          {pos.yB !== pos.y && pos.xB !== pos.x && <img
+            key={posId + 'c'}
+            id='asteroid-shadow'
+            alt='asteroid-sprite'
+            src={require('../../assets/img/asteroid_large_sprt.png')}
+            style={motion(pos.xB, pos.yB, pos.dir)} />}
+     
+        </>
         ) : (
           ""
         );
