50698e48aa576bd756a503808653a7df41f4dc92
Chris <cangalet@gmail.com>
2022-04-16 21:30:17 -0700

    added HUDFooter with logout and quit buttons to logout user and take user back to start page

--- client/src/components/HudFooter/index.js
+++ client/src/components/HudFooter/index.js
@@ -1,46 +1,52 @@
 import * as React from "react";
 import { Box, Grid, Typography,  } from "@mui/material";
+import Auth from "../../util/auth";
 import { useHistory } from 'react-router-dom';
 
-const navigate = useHistory();  
+export default function HudFooter() {
+    const navigate = useHistory();  
 
-const handleLogout = () => {
-    Auth.logout();
-    navigate.push("/");
-  }
+    const handleQuit = () => {
+        navigate.push("/start");
+    }
 
-export default function Hud() {
-  return (
-    <Box
-        component="footer"
-        sx={{
-          px: 3,
-          mt: 'auto',
-        }}
-    >
-        <Grid container spacing={2} direction="row" alignItems="center">
-            <Grid item xs={9}  >
-                {/* Controls */}
-                <Typography variant="h6" align="left">
-                    Press W: Up | A:Left | D:Right
-                </Typography>
+    const handleLogout = () => {
+        Auth.logout();
+        navigate.push("/");
+    }
+
+    return (
+        <Box
+            component="footer"
+            sx={{
+            px: 3,
+            mt: 'auto',
+            }}
+        >
+            <Grid container spacing={2} direction="row" alignItems="center">
+                <Grid item xs={9}  >
+                    {/* Controls */}
+                    <Typography variant="h6" align="left">
+                        Press W: Up | A:Left | D:Right
+                    </Typography>
+                </Grid>
+                <Grid item xs={3} align="center">
+                    <Box display="flex" justifyContent="space-between" sx={{ mx: 10 }}>
+                        <button 
+                        type="button" 
+                        className="nes-btn upperCase"
+                        onClick={handleQuit}>
+                        Quit
+                        </button>
+                        <button 
+                        type="button" 
+                        className="nes-btn upperCase"
+                        onClick={handleLogout}>
+                        Logout
+                        </button>
+                    </Box>
+                </Grid>
             </Grid>
-            <Grid item xs={3} align="center">
-                <Box display="flex" justifyContent="space-between" sx={{ mx: 10 }}>
-                    <button 
-                    type="button" 
-                    className="nes-btn upperCase">
-
-                    Quit
-                    </button>
-                    <button 
-                    type="button" 
-                    className="nes-btn upperCase">
-                    Logout
-                    </button>
-                </Box>
-            </Grid>
-        </Grid>
-    </Box>   
+        </Box>   
 );
 }
