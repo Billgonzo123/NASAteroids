062df9ea9594e8d54dc2afbb52d5ffff5823568b
CKURZ <ckurz098@gmail.com>
2022-04-17 11:27:32 -0700

    Added gameover sound and playe deth sound

--- client/src/components/MainWindow/index.js
+++ client/src/components/MainWindow/index.js
@@ -143,7 +143,7 @@ console.log(gameState)
         <audio id="engine_snd" src={require(`../../assets/snd/player_snd/engine_snd.wav`)} loop type="audio/wav"/>
         <audio id="bullet_snd" src={require(`../../assets/snd/bullet_snd/bullet_snd.wav`)}  type="audio/wav"/>
         <audio id="player_die" src={require(`../../assets/snd/player_snd/player_die.wav`)}  type="audio/wav"/>
-        <audio id="player_die" src={require(`../../assets/snd/player_snd/gameover.wav`)}  type="audio/wav"/>
+        <audio id="gameover" src={require(`../../assets/snd/player_snd/gameover.wav`)}  type="audio/wav"/>
 
         
         {/*------------- HUD  -------------*/}
