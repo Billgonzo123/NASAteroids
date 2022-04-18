5234750222b2fe4c1b30bc72494d515409d539af
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-04-18 11:52:30 -0700

    preliminary logic for adding to user highscore array

--- client/src/components/GameStats/index.js
+++ client/src/components/GameStats/index.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-} from '@mui/material';
+// import {
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableContainer,
+//   TableRow,
+// } from '@mui/material';
 import { GET_ME, GET_LEADERBOARD } from '../../util/queries';
 import {
   ADD_USER_HIGHSCORE,
@@ -14,21 +14,37 @@ import {
 } from '../../util/mutations';
 
 const GameOverStats = ({ gameState }) => {
+  //leaderboard data
   const { data: leaderboardData } = useQuery(GET_LEADERBOARD);
-  const { data: userData } = useQuery(GET_ME);
+  const leaderboardHighscores = leaderboardData.leaderboard.highscores;
+  console.log('leaderboardHighscores', leaderboardHighscores);
 
-  // const [addUserHighscore, { highscoreData }] = useMutation(ADD_USER_HIGHSCORE);
-  // const [addLeaderboardHighscore, { addLeaderboardData }] = useMutation(
-  //   ADD_LEADERBOARD_HIGHSCORE
-  // );
-  console.log('leaderboard data', leaderboardData);
-  console.log('userData', userData);
-  console.log('gameState', gameState.score);
-  //loop through userdata, if current score is higher, add, sort and pop
+  //logged in user data
+  const { data: userData } = useQuery(GET_ME);
+  const userHighscores = userData.me.highscores;
+  console.log('userHighscores', userHighscores);
+
+  //current score
+  const currentScore = gameState.score;
+  console.log(gameState.score);
+
+  //*loop through userdata, if current score is higher, add, sort and pop
+  //* if equal to another score, don't add (error handling)
+
+  //if user has a highscore greater than the currentscore, better luck next time
+  if (userHighscores.find((score) => score > currentScore)) {
+    console.log("better luck next time")
+  } else {
+    //use mutation to add to their highscore array
+    console.log("congrats!")
+  }
 
   return (
-    <TableContainer>
-      {/* <Table sx={{ textTransform: 'uppercase' }} aria-label="simple table">
+    <>
+    hello!
+    </>
+    // <TableContainer>
+      /* <Table sx={{ textTransform: 'uppercase' }} aria-label="simple table">
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name} sx={{ '& td': { border: 0 } }}>
@@ -41,8 +57,8 @@ const GameOverStats = ({ gameState }) => {
             </TableRow>
           ))}
         </TableBody>
-      </Table> */}
-    </TableContainer>
+      </Table> */
+    /* </TableContainer> */
   );
 };
 
