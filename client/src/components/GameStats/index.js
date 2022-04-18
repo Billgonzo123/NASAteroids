96d406a10175bb1787f696e397aee422a19e84e3
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-04-18 11:18:15 -0700

    isolated needed data: leaderboard, user, current game

--- client/src/components/GameStats/index.js
+++ client/src/components/GameStats/index.js
@@ -1,38 +1,49 @@
 import * as React from 'react';
-import { useQuery, useMutation } from "@apollo/client";
-import {Table ,TableBody, TableCell, TableContainer, TableRow} from '@mui/material';
-import { GET_ME, GET_LEADERBOARD } from "../../util/queries";
-import { ADD_USER_HIGHSCORE, ADD_LEADERBOARD_HIGHSCORE } from "../../util/mutations";
+import { useQuery, useMutation } from '@apollo/client';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from '@mui/material';
+import { GET_ME, GET_LEADERBOARD } from '../../util/queries';
+import {
+  ADD_USER_HIGHSCORE,
+  ADD_LEADERBOARD_HIGHSCORE,
+} from '../../util/mutations';
 
-function createData(name, score) {
-  return { name, score};
-}
+const GameOverStats = ({ gameState }) => {
+  const { data: leaderboardData } = useQuery(GET_LEADERBOARD);
+  const { data: userData } = useQuery(GET_ME);
 
-const rows = [
-  createData('score:', 534),
-  createData('XP:', 20),
-  createData('bonus:', 1),
-  createData('total:', 555),
-];
+  // const [addUserHighscore, { highscoreData }] = useMutation(ADD_USER_HIGHSCORE);
+  // const [addLeaderboardHighscore, { addLeaderboardData }] = useMutation(
+  //   ADD_LEADERBOARD_HIGHSCORE
+  // );
+  console.log('leaderboard data', leaderboardData);
+  console.log('userData', userData);
+  console.log('gameState', gameState.score);
+  //loop through userdata, if current score is higher, add, sort and pop
 
-export default function GameOverStats() {
   return (
     <TableContainer>
-      <Table sx={{ textTransform: 'uppercase' }} aria-label="simple table">
+      {/* <Table sx={{ textTransform: 'uppercase' }} aria-label="simple table">
         <TableBody>
           {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '& td': { border: 0 } }}
-            >
+            <TableRow key={row.name} sx={{ '& td': { border: 0 } }}>
               <TableCell scope="row" align="left" sx={{ p: 0.25 }}>
                 {row.name}
               </TableCell>
-              <TableCell align="center" sx={{ p: 0.25 }}>{row.score}</TableCell>
+              <TableCell align="center" sx={{ p: 0.25 }}>
+                {row.score}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </Table> */}
     </TableContainer>
   );
-}
+};
+
+export default GameOverStats;
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -4,26 +4,29 @@ import Profile from '../components/Profile';
 import Leaderboard from '../components/Leaderboard';
 import Footer from '../components/Footer';
 import { Box, Grid, Card, CardActions } from '@mui/material';
-import Auth from "../util/auth";
+import Auth from '../util/auth';
+// TESTING
+import GameStats from '../components/GameStats';
 
-const Start = ({gameState, setGameState}) => {
-
+const Start = ({ gameState, setGameState }) => {
   const navigate = useHistory();
 
   const handleStart = () => {
-    navigate.push("/main");
-  }
+    navigate.push('/main');
+  };
   const handleLogout = () => {
     Auth.logout();
-    navigate.push("/");
-  }
+    navigate.push('/');
+  };
 
   if (!Auth.loggedIn()) {
-    return <Redirect to="/" />
+    return <Redirect to="/" />;
   }
 
   return (
     <Box>
+      {/* GAMESTATS TESTING ONLY */}
+      <GameStats gameState={gameState}/>
       <Grid
         container
         columnSpacing={{ md: 1 }}
@@ -41,39 +44,36 @@ const Start = ({gameState, setGameState}) => {
           direction="row"
           justifyContent="center"
         >
-          <Profile 
-            setGameState={setGameState}
-            gameState={gameState}
-          />
+          <Profile setGameState={setGameState} gameState={gameState} />
           <Leaderboard />
         </Grid>
         <CardActions
-            sx={{
-              justifyContent: "space-between",
-              backgroundColor: "transparent",
-              mt: 10,
-              gap: 20
-            }}
+          sx={{
+            justifyContent: 'space-between',
+            backgroundColor: 'transparent',
+            mt: 10,
+            gap: 20,
+          }}
+        >
+          <button
+            type="button"
+            className="nes-btn upperCase"
+            onClick={handleStart}
           >
-            <button 
-              type="button" 
-              className="nes-btn upperCase"
-              onClick={handleStart}
-              >
-              Start
-            </button>
-            <button 
-              type="button" 
-              className="nes-btn upperCase"
-              onClick={handleLogout}>
-              Logout
-            </button>
-          </CardActions>
+            Start
+          </button>
+          <button
+            type="button"
+            className="nes-btn upperCase"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </CardActions>
         <Footer />
       </Grid>
     </Box>
-
-  )
+  );
 };
 
 export default Start;
