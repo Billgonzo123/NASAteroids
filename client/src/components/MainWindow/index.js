66d385823ac604d2a0de60e5e03a2d03444f34fa
CKURZ <ckurz098@gmail.com>
2022-04-18 18:30:41 -0700

    Fixed 300 points at start. Now 0

--- client/src/components/MainWindow/index.js
+++ client/src/components/MainWindow/index.js
@@ -58,11 +58,8 @@ const MainWindow = ({ gameState, setGameState }) => {
         if (bullets) return updateBullet(oldPositions);
         return null;
       });
-    
       //check for a change in screen size and change scale if change
       checkScreenScale(screenWidth, setScreenScale);
-
-
       if (globalPlayer.alive && spaceDown.current === 1 && bullets.length <= 5) {
         spaceDown.current = 2;
         playSoundCancel('bullet_snd');
@@ -71,7 +68,9 @@ const MainWindow = ({ gameState, setGameState }) => {
       }
       //asteroidGeneration
       if (numOfAst.current <= 0) {
+        if (gameState.curLevel){
         (gameState.timer <= 60) ? setGameState(old => ({ ...old, curLevel: old.curLevel + 1, timer: 0, score: (old.score + 3000) })) : setGameState(old => ({ ...old, curLevel: old.curLevel + 1, timer: 0, score: (old.score + 1000) }));
+        }
         setAsteroids(asteroidGeneration(asteroids, globalPlayer, 2, gameState.curLevel + 1, 0, 0, 1));
       }
       checkBulletCollision(bullets, setBullets, setAsteroids, asteroids, globalPlayer, setGameState);
