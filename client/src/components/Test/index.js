d1f36d461dbcc83b23aeeabb0b19891c53e90bc8
Emily Dokken <dokkenemily@gmail.com>
2022-04-20 11:25:18 -0700

    add test data

--- client/src/components/Test/index.js
+++ client/src/components/Test/index.js
@@ -1,11 +1,72 @@
 import React from "react";
+import {Typography, Grid, Container } from "@mui/material";
+import { GET_ME } from "../../util/queries";
+import { useQuery } from "@apollo/client";
 
 
 const Test = () => {
+  const currentScore = 8000987;
+  const userScoreDisplay = [
+    {score: 50, date: '04/20/22'},
+    {score: 100, date: '04/20/22'},
+    {score: 200, date: '04/20/22'},
+  ];
+
+  const leaderboardData = [
+    {score: 50, date: '04/20/22'},
+    {score: 100, date: '04/20/22'},
+    {score: 200, date: '04/20/22'},
+  ];
+
+  
+
   return (
 
 
-    <h1>Test page</h1>
+    <Container maxWidth="md">
+    <Typography variant="subtitle1" align="center" sx={{mt: 2}}>
+      Final Score: {currentScore}
+    </Typography>
+    <Grid container spacing={4} sx={{ padding: 6, }}>
+      <Grid item xs={6} align="center"> 
+        Your Highscores:
+      </Grid>
+      <Grid item xs={6} align="center">
+        Leaderboard:
+      </Grid>
+      <Grid item xs={6} align="center">
+        <Grid container spacing={1}>
+          {Object.keys(userScoreDisplay).map((index) => {
+            const score = userScoreDisplay[index];
+            return userScoreDisplay ? (
+              <>
+                <Grid item xs={12} key={index}>
+                  {score.date}....{score.score}
+                </Grid>
+              </>
+            ) : (
+              ''
+            );
+          })}
+        </Grid>
+      </Grid>
+      <Grid item xs={6} align="center">
+        <Grid container spacing={1}>
+          {leaderboardData.map(({ score, date }) => {
+            return leaderboardData ? (
+              <>
+                <Grid item xs={12} key={score}>
+                  {date}....{score}
+                </Grid>
+              </>
+            ) : (
+              ''
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Grid>
+  </Container>
   )
 };
 
