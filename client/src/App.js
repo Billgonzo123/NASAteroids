8911748c7466fb8dc735356f455dcac438e1d7d3
Emily Dokken <dokkenemily@gmail.com>
2022-04-20 18:33:26 -0700

    add scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
-});
+}); 
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
