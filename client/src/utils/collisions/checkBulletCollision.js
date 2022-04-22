f4a7b41eaa1c1b30fc2a6f1391fe0bc4baff51eb
CKURZ <ckurz098@gmail.com>
2022-04-22 16:50:12 -0700

    Fixed UFO points issue

--- client/src/utils/collisions/checkBulletCollision.js
+++ client/src/utils/collisions/checkBulletCollision.js
@@ -1,6 +1,6 @@
 import getDistance from '../gameUtils/getDistance'
 import destoryAsteroid from '../createObjects/destoryAsteroid';
-import {stopSound} from '../playSound';
+import { stopSound } from '../playSound';
 
 
 function checkBulletCollision(bullets, setBullets, setAsteroids, asteroids, globalPlayer, setGameState, ufo, setUfo) {
@@ -8,41 +8,47 @@ function checkBulletCollision(bullets, setBullets, setAsteroids, asteroids, glob
 
 
     if (bullets.length) {
+        if (ufo.x > 0) {
+            bullets.map((b, i) => {
+                const ufoX = ufo.x;
+                const ufoY = ufo.y;
+                const lineC = (ufoX > 0 && ufoY < 1920) ? getDistance(b.x, ufoX, b.y, ufoY) : 200;
+                if (lineC < 30) {
+                    const newBullets = bullets;
+                    const results = newBullets.filter((bullet, index) => index !== i);
+                    stopSound('ufo_snd')
+                    setBullets(results);
+                    setUfo(old => ({ ...old, x: 2000 }))
+                    setGameState(old => ({ ...old, score: (old.score + (3000)) }));
+                }
+                return false;
+            });
+      
+        }
+
         Object.keys(asteroids).map((id) => {
             //bullet has no radius
             const a = asteroids[id];
-            const ufoX = ufo.x;
-            const ufoY = ufo.y; 
-           
+
             if (a.alive) {
                 //radius is hard coded
                 const astRadius = [23, 62, 124];
                 const r = astRadius[a.size];
-                const dist = r+5;
+                const dist = r + 5;
                 const bonusScores = [100, 50, 20];
                 const bonus = bonusScores[a.size]
-                
+
                 bullets.map((b, i) => {
                     //Each bullet gets 2 lines of detection for x,y,xB,yB
                     const lineA = getDistance(b.x, a.x + r, b.y, a.y + r);
                     const lineB = (a.x !== a.xB || a.y !== a.yB) ? getDistance(b.x, a.xB + r, b.y, a.yB + r) : 200;
-                    const lineC = (ufoX > 0 && ufoY<1920) ? getDistance(b.x, ufoX , b.y, ufoY ) : 200;
-                    if (lineC < 30) {
-                        const newBullets = bullets;
-                        const results = newBullets.filter((bullet, index) => index !== i);
-                        stopSound('ufo_snd')
-                        setBullets(results);
-                        setUfo(old => ({...old, x: 2000}))
-                        setGameState(old => ({...old, score: (old.score+(1000))}));
-                        return false;
-                    }
 
                     if (lineA < dist || lineB < dist) {
                         const newBullets = bullets;
                         const results = newBullets.filter((bullet, index) => index !== i);
                         setBullets(results);
                         destoryAsteroid(id, globalPlayer, asteroids, setAsteroids);
-                        setGameState(old => ({...old, score: (old.score+(bonus))}));
+                        setGameState(old => ({ ...old, score: (old.score + (bonus)) }));
                         return false;
                     };
                     return false;
