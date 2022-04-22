21d3c86159338249a21cc96b931c12a6dec31eae
Emily Dokken <dokkenemily@gmail.com>
2022-04-22 15:06:40 -0700

    add back button to mobile view

--- client/src/components/MobileMainWindow/index.js
+++ client/src/components/MobileMainWindow/index.js
@@ -1,17 +1,25 @@
-import * as React from 'react';
-import { Typography, Container } from '@mui/material';
+import * as React from "react";
+import { Typography, Container } from "@mui/material";
 
 export default function MobileMainWindow() {
+  const handleBack = () => {
+    window.location = "/start";
+}
   return (
-  <Container component="main" sx={{mb: 2 }} maxWidth="lg">
-    <Typography
-      variant="h3"
-      align="center"
-      gutterBottom
-      sx={{ textTransform: 'uppercase', py:2, mt: 10, fontSize: '30px' }}
-    >
-      Houston, we've got an error! This game is not compatible on mobile device!
-    </Typography>
-  </Container>
-);
+
+    <Container component="main" sx={{ mb: 2 }} maxWidth="lg">
+      <Typography
+        variant="h3"
+        align="center"
+        gutterBottom
+        sx={{ textTransform: "uppercase", py: 2, mt: 10, fontSize: "30px" }}
+      >
+        Houston, we've got an error! This game is not compatible on mobile
+        device!
+      </Typography>
+      <button type="button" className="nes-btn upperCase" onClick={handleBack}>
+        Quit
+      </button>
+    </Container>
+  );
 }
